0f11e4537cfe7ace8d80e5a216e2b923169c37b1
Nitin Narasappa <nitingn@gmail.com>
2016-03-26 10:33:17 +1100

    Update BalancedString.java
    Added more comments.

--- BalancedString.java
+++ BalancedString.java
@@ -11,13 +11,14 @@ import java.util.regex.*;
 * Output: For each case, print 'true' if the string is balanced, 'false' otherwise.
 *
 * Examples of some correctly balanced strings are: "{}()", "[{()}]", "({()})" 
-* Examples of some unbalanced strings are: "{}(", "({)}", "[[", "}{" etc.
+* Examples of some unbalanced strings are: "{}(", "({)}", "[[", "}{"
 */
 public class BalancedString {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         while (reader.hasNext()) {
+            // Check if the given string is balanced. Print true if balanced; false otherwise.
             if(isBalanced(reader.next())){
                 System.out.println("true");
             }
@@ -26,12 +27,18 @@ public class BalancedString {
             }
         }
     }
-    
+    /**
+     * Method to check if the string is balanced.
+     */
     private static boolean isBalanced(String str){
+        // Empty string is balanced.
         if(str.length() == 0) {
             return true;
         }
         else {
+            // Make use of stack (LIFO) for checking the logic.
+            // Push the opening brackets on to the stack
+            // and match the closing brackets with top of the stack.
             Stack stack = new Stack();
             for(char ch: str.toCharArray()) {
                 switch(ch) {
@@ -57,6 +64,7 @@ public class BalancedString {
                     break;
                 }
             }
+            // This is incomplete string.
             return stack.isEmpty();
         }
     }
